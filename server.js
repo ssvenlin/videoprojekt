4f775ae7cc1c9fa5eb4ce25b4e647cdfc2ab6d34
ssvenlin <sandra.svenlin@gmail.com>
2020-04-16 11:30:13 +0300

    save

--- server.js
+++ server.js
@@ -11,6 +11,27 @@ app.get('/', function (req, res){
   res.send("Hello World");
 });
 
+app.get('/getid', function (req, res){
+  let sql = "SELECT * FROM media";
+  
+  let db = new sqlite3.Database('media.db', (err) => {
+    if (err) {
+      console.error(err.message);
+    }
+    console.log('Connected to the media database.');
+  });
+
+  db.all(sql, (err,rows) => {
+    res.json({username: rows})
+  });
+
+  db.close((err) => {
+    if (err){
+    console.log(err.message);
+  }
+  console.log('Close the database connection.');
+  });   
+});
 
 let db = new sqlite3.Database('media.db', (err) => {
     if (err) {
@@ -18,6 +39,25 @@ let db = new sqlite3.Database('media.db', (err) => {
     }
     console.log('Connected to the media database.');
   });
+
+  db.run(`INSERT INTO media(id, name) VALUES(?,?)`, function(err) {
+ 
+    if (err) {
+      return console.log(err.message);
+    }
+    // get the last insert id
+    console.log(`A row has been inserted with rowid ${this.lastID}`);
+  });
+
+  //close database connection
+  db.close((err) => {
+    if (err) {
+      console.error(err.message);
+    }
+    console.log('Close the database connection.');
+  });
+
+
 
 var server = app.listen(portnummer, function(){
 
